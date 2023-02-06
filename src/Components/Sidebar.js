aeb2b763103fa2b01ed49ea957b1ec6741c34406
NoorulAmeenJ <noorularafat786@gmail.com>
2023-02-06 08:09:10 +0530

    started

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const Sidebar = () => {
+  return (
+    <div className='practise'>
+            <div className='sidebar'>Sidebar
+         <ul>
+            <li>dashboard</li>
+            <li>interface </li>
+            <li>components
+                 <ul>
+                    <li>buttons</li>                        
+                    <li>cards</li>
+                <select>
+                <option>buttons</option>
+                <option>cards</option>
+            
+                </select>
+                </ul>
+            </li>
+         </ul>
+      
+            </div>
+            <div className='fullbar'>fullbar</div>
+           
+    </div>
+
+  )
+}
+
+export default Sidebar
