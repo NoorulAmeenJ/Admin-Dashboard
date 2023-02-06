855a32dcce482688131ca621dbd4cb27cb6e523b
NoorulAmeenJ <noorularafat786@gmail.com>
2023-02-06 20:15:16 +0530

    side bar going on

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,26 +1,81 @@
 import React from 'react'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 const Sidebar = () => {
   return (
     <div className='practise'>
             <div className='sidebar'>Sidebar
-         <ul>
-            <li>dashboard</li>
-            <li>interface </li>
-            <li>components
-                 <ul>
-                    <li>buttons</li>                        
-                    <li>cards</li>
-                <select>
-                <option>buttons</option>
-                <option>cards</option>
-            
-                </select>
-                </ul>
-            </li>
-         </ul>
-      
+            <div>
+            <div>
+            SB Admin
+          </div>
+          <hr></hr>
+            <div>
+           Dashboard
+          </div>
+          <hr></hr>
+          <div>
+       Interface
+          </div>
+        
+      <Accordion class Name='try'>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>Components</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>Custom Components</div>
+          <Typography>Cards</Typography>
+          <Typography>Buttons</Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion class Name='try'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Utilities</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>Custom Utilities</div>
+          <Typography>Colors</Typography>
+          <Typography>Borders</Typography>
+          <Typography>Animations</Typography>
+          <Typography>Others</Typography>
+        </AccordionDetails>
+      </Accordion>
+     <hr></hr>
+     <div>
+       Interface
+          </div>
+     <Accordion class Name='try'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Pages</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>LOGIN SCREENS</div>
+          <Typography>Login</Typography>
+          <Typography>Register</Typography>
+          <Typography>Forgot Password</Typography>
+          <div>OTHER PAGE</div>
+          <Typography>404 Pages</Typography>
+          <Typography>Blank Page</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Typography>Charts</Typography>
+      <Typography>Tables</Typography>
+      <hr></hr>
+    </div>
             </div>
             <div className='fullbar'>fullbar</div>
            
