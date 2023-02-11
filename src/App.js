e162ce19faa0a140aeb23492c1a363afac366418
NoorulAmeenJ <noorularafat786@gmail.com>
2023-02-11 08:10:27 +0530

    components page completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import InvalidUrl from './Components/InvalidUrl';
 import Dashboard from './Components/Dashboard';
 import Base from './Base';
 import Buttons from './Components/Buttons';
+import Animations from './Components/Animations';
+import BlankPage from './Components/BlankPage';
+import Borders from './Components/Borders';
+import Cards from './Components/Cards';
+import Charts from './Components/Charts';
+import Colors from './Components/Colors';
+import ForgotPassword from './Components/ForgotPassword';
+import Login from './Components/Login';
+import Others from './Components/Others';
+import Register from './Components/Register';
+import Tables from './Components/Tables';
+import ErrorPage from './Components/ErrorPage';
 
 function App() {
   return (
@@ -16,12 +28,62 @@ function App() {
             <Dashboard/>
         </Route>
 
-        <Route path="/buttons">
-           <Buttons/>
+        <Route path="/Animations">
+           <Animations/>
         </Route>
+
+        <Route path="/BlankPage">
+              <BlankPage/>
+           </Route>
+
+           <Route path="/Borders">
+              <Borders/>
+           </Route>
+
+           <Route path="/Buttons">
+           <Buttons/>
+            </Route>
+
+           <Route path="/Cards">
+             <Cards/>
+           </Route>
+
+           <Route path="/Charts">
+              <Charts/>
+           </Route>
+
+           <Route path="/Colors">
+              <Colors/>
+           </Route>
+
+           <Route path="/ForgotPassword">
+              <ForgotPassword/>
+           </Route>
+
+           <Route path="/ErrorPage">
+              <ErrorPage/>
+           </Route>
+
+           <Route path="/Login">
+              <Login/>
+           </Route>
+
+           <Route path="/Others">
+              <Others/>
+           </Route>
+
+           <Route path="/Register">
+                <Register/>
+           </Route>
+
+           <Route path="/Tables">
+                <Tables/>
+           </Route>
+
         <Route path="**">
              <InvalidUrl/>
         </Route>
+
      </Switch>
 
     </div>
---
+++ src/Components/Borders.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Borders = () => {
+  return (
+    <div>Borders</div>
+  )
+}
+
+export default Borders
